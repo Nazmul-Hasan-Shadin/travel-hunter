385ba79801050353b95cc1c27b55463c3b06a069
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-01-25 13:56:03 +0600

    porject initialized

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/Routes.jsx'
+import Home from './pages/Home/Home/Home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <div className='max-w-[1920px] mx-auto'>
+   <RouterProvider router={router}>
+        <Home></Home>
+   </RouterProvider>
+   </div>
+  </React.StrictMode>,
+)

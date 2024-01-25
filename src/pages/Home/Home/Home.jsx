443fe06866f1d58b33819b2d4b8627f5c54958ef
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-01-25 22:10:59 +0600

    brand logo added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../../shared/Navbar/Navbar';
 import Header from '../Header/Header';
+import BrandLogo from '../../../shared/BrandLogo/BrandLogo';
 
 const Home = () => {
     return (
         <div>
            <Header></Header>
+           <BrandLogo></BrandLogo>
         </div>
     );
 };
---
+++ src/shared/BrandLogo/BrandLogo.jsx
@@ -0,0 +1,20 @@
+import brandlogo1 from "../../assets/images/brand/FedExLogo.svg"
+import brandlogo2 from "../../assets/images/brand/Hubspot Logo.svg"
+import brandlogo3 from "../../assets/images/brand/logo.png"
+import brandlogo4 from "../../assets/images/brand/logo1.svg"
+import brandlogo5 from "../../assets/images/brand/Walmart Logo.svg"
+const BrandLogo = () => {
+    return (
+        <div className="bg-[#008080] ">
+            <div className="flex gap-20 justify-center py-9 ">
+                <img src={brandlogo1} alt="" />
+                <img src={brandlogo2} alt="" />
+                <img src={brandlogo3} alt="" />
+                <img src={brandlogo4} alt="" />
+                <img src={brandlogo5} alt="" />
+                </div>
+        </div>
+    );
+};
+
+export default BrandLogo;
